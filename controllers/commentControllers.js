1693d4ce701dd5e0f75750d778b9a68d06716cae
Destroyerofdarkness <dawidadogier@gmail.com>
2026-04-05 21:11:43 +0200

    Added route for getting the view comments

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -5,17 +5,45 @@ const comment_register = async (req, res) => {
   try {
     console.log("Registering comment with:", BODY);
     await Comment.publish(BODY);
-    res.status(200).json({success:true, message: "Succesfully published the comment"});
+    res
+      .status(200)
+      .json({ success: true, message: "Succesfully published the comment" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      err,
+      success: false,
+      message: "Unable to insert the comment into the database!!",
+    });
+  }
+};
+
+const send_view_comments = async (req, res) => {
+  const id = req.params.id;
+  try {
+    console.log(id)
+    const viewComments = await Comment.find({connection:id});
+    if(viewComments.length === 0){
+      throw Error("No comments found!!")
+    }
+    console.log(viewComments)
+    res
+      .status(200)
+      .json({
+        viewComments,
+        success: true,
+        message: "Found the view comments succesfully"
+      });
   } catch (err) {
     console.log(err);
     res
-      .status(400)
+      .status(404)
       .json({
-        err,
+        err: err.message,
         success: false,
-        message: "Unable to insert the comment into the database!!",
+        message: "Failed to find comments for the given view",
       });
   }
 };
 
-module.exports = {comment_register}
+module.exports = { comment_register, send_view_comments };
--- routes/comment.js
+++ routes/comment.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/commentControllers");
 
 router.post("/register", controller.comment_register);
 
+router.get("/:id", controller.send_view_comments)
+
 
 module.exports = router;
