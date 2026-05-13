c881fdc5a0aff5086456a295dd8362868f2e62b1
Destroyerofdarkness <dawidadogier@gmail.com>
2026-05-13 10:57:32 +0200

    Made the model for reports of the views and also made a route for creating the reports

---
+++ controllers/reportController.js
@@ -0,0 +1,15 @@
+const Report = require("../models/Report");
+
+const report_create = async(req,res)=>{
+    const {BODY} = req.body
+    try {
+        await Report.publish(BODY);
+        res.status(200).json({success:true, message: "Succesfully published the report for the view!!"})
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({err, success:false, message: "Couldn't create the report because of errors!!"})
+    }
+}
+
+
+module.exports = {report_create}
